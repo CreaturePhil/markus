67933c43a7975a212e02787f1fd04497e65ffa7b
Phil <birkal@outlook.com>
2014-09-21 14:49:19 -0700

    Add fontawesome

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,7 @@ var concatCss = require('gulp-concat-css');
 var minifyCss = require('gulp-minify-css');
 
 gulp.task('default', function() {
-  gulp.src(['public/css/bootstrap.min.css', 'public/css/custom.css'])
+  gulp.src(['public/css/bootstrap.min.css', 'public/css/font-awesome.min.css','public/css/custom.css'])
     .pipe(concatCss('main.min.css'))
     .pipe(minifyCss())
     .pipe(gulp.dest('public/css/'));
