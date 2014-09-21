c582271630b6addfc09534183230234e4ea85edc
Phil <birkal@outlook.com>
2014-09-20 19:25:16 -0700

    Setting up application

---
+++ app.js
@@ -0,0 +1,101 @@
+var express = require('express');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var compress = require('compression');
+var session = require('express-session');
+var csrf = require('lusca').csrf();
+var methodOverride = require('method-override');
+
+var _ = require('lodash');
+var MongoStore = require('connect-mongo')({ session: session });
+var flash = require('express-flash');
+var path = require('path');
+var mongoose = require('mongoose');
+var passport = require('passport');
+var expressValidator = require('express-validator');
+
+var app = express();
+
+/**
+ * App configuration.
+ */
+
+// view engine setup
+//app.set('views', path.join(__dirname, 'app/views'));
+//app.set('view engine', 'jade');
+
+app.use(compress());
+app.use(favicon(__dirname + '/public/favicon.ico'));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(expressValidator());
+app.use(methodOverride());
+app.use(cookieParser());
+//app.use(session({
+//  resave: true,
+//  saveUninitialized: true,
+//  secret: secrets.sessionSecret,
+//  store: new MongoStore({
+//    url: secrets.db,
+//    auto_reconnect: true
+//  })
+//}));
+//app.use(passport.initialize());
+//app.use(passport.session());
+//app.use(flash());
+
+// csrf whitelist
+//var csrfExclude = ['/url1', '/url2'];
+//app.use(function(req, res, next) {
+//  // CSRF protection.
+//  if (_.contains(csrfExclude, req.path)) return next();
+//  csrf(req, res, next);
+//});
+
+// time for static cache
+var hour = 3600000;
+var day = hour * 24;
+var week = day * 7;
+app.use(express.static(path.join(__dirname, 'public'), { maxAge: week }));
+
+app.get('/', function(req, res) {
+  res.send('Hello World!');
+});
+
+/// catch 404 and forward to error handler
+//app.use(function(req, res, next) {
+//    var err = new Error('Not Found');
+//    err.status = 404;
+//    next(err);
+//});
+
+/**
+ * Error Handlers
+ */
+
+// development error handler
+// will print stacktrace
+//if (app.get('env') === 'development') {
+//    app.use(function(err, req, res, next) {
+//        res.status(err.status || 500);
+//        res.render('error', {
+//            message: err.message,
+//            error: err
+//        });
+//    });
+//}
+
+// production error handler
+// no stacktraces leaked to user
+//app.use(function(err, req, res, next) {
+//    res.status(err.status || 500);
+//    res.render('error', {
+//        message: err.message,
+//        error: {}
+//    });
+//});
+
+module.exports = app;
